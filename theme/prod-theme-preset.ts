648aa4a28424a85b7790e002d5ab346eac588e3c
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2025-01-10 04:48:09 +0100

    Update sources

--- theme/prod-theme-preset.ts
+++ theme/prod-theme-preset.ts
@@ -3,5 +3,5 @@ import * as PatchKitLauncher from "@upsoft/patchkit-launcher-theme-package-dev-t
 export default {
   id: `7ddd0afd-5c8e-405a-bcab-4de1f0ee20f4`,
   distDirPath: `dist`,
-  runtimeMinVersionLabel: `2.0.0`,
+  runtimeMinVersionLabel: `2.1.0`,
 } satisfies PatchKitLauncher.ThemePreset;
