2429c41bb29875ca717ded5e735665775a954608
Beniamin Batkowski <bencrow96@gmail.com>
2026-04-13 13:03:02 +0200

    Use dynamic year in copyright footer

--- theme/src/routes/user-is-authenticated/index.tsx
+++ theme/src/routes/user-is-authenticated/index.tsx
@@ -39,7 +39,7 @@ export function RouteComponent() {
             {" "}
             to start distributing your games today!
           </TypographyP>
-          <TypographyMuted>Upsoft 2024</TypographyMuted>
+          <TypographyMuted>{`Upsoft ${new Date().getFullYear()}`}</TypographyMuted>
         </div>
       </ScrollArea>
     </div>
